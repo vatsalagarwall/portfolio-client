0670a4c43985f33029ed2ece35803370f85737c1
Vatsal Agarwal <103558712+vatsalagarwall@users.noreply.github.com>
2024-02-28 18:20:47 +0530

    Update Contact.jsx

--- src/Components/Contact.jsx
+++ src/Components/Contact.jsx
@@ -11,7 +11,7 @@ function Contact() {
   const handleSubmit = (e) => {
     e.preventDefault();
     axios
-      .post("http://localhost:8080", {
+      .post("https://portfolio-server-q68j.onrender.com", {
         name,
         email,
         message,
