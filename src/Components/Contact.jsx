bf2c3438b7ad60c21551e357940a1db8ccb99f22
Vatsal Agarwal <bindalvatsaljpr@gmail.com>
2024-09-09 21:06:10 +0530

    commit

--- src/Components/Contact.jsx
+++ src/Components/Contact.jsx
@@ -157,7 +157,7 @@ function Contact() {
               </svg>
             </a>
 
-            <a
+            {/* <a
               href="https://twitter.com/vatsalagarwalll"
               target="_blank"
               style={{ color: "white", marginLeft: "30px" }}
@@ -176,7 +176,7 @@ function Contact() {
               >
                 <path d="M22 4s-.7 2.1-2 3.4c1.6 10-9.4 17.3-18 11.6 2.2.1 4.4-.6 6-2C3 15.5.5 9.6 3 5c2.2 2.6 5.6 4.1 9 4-.9-4.2 4-6.6 7-3.8 1.1 0 3-1.2 3-1.2z" />
               </svg>
-            </a>
+            </a> */}
           </div>
           <br />
           <button
