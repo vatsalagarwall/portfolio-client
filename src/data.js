5c27853a02a704cfd20b05b2cf662df65c75542c
Vatsal Agarwal <bindalvatsaljpr@gmail.com>
2024-09-09 21:02:06 +0530

    commit

--- src/data.js
+++ src/data.js
@@ -5,15 +5,15 @@ export const data = [{
     "link": "https://chitchat12.netlify.app/",
     "github": "https://github.com/vatsalagarwall?tab=repositories"
 }, {
-    "name": "Fresco",
-    "img": "https://logowik.com/content/uploads/images/fresco6300.jpg",
-    "description": "Optimized restaurant website for enhanced user experience, featuring interactive menus, efficient table reservations, and promotions. Improved customer engagement with streamlined online services.",
-    "link": "https://fresco12.netlify.app/",
-    "github": "https://github.com/vatsalagarwall/fresco-github"
-}, {
     "name": "Expenzify",
     "img": "https://plus.unsplash.com/premium_photo-1679470210717-97fc30968fdf?w=600&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxlZGl0b3JpYWwtZmVlZHwyfHx8ZW58MHx8fHx8",
     "description": "A comprehensive expense tracking and management app that simplifies the process of recording, categorizing, and reporting your expenses.",
     "link": "https://expenzify.vercel.app/",
     "github": "https://github.com/vatsalagarwall/expenzify"
+}, {
+    "name": "Fresco",
+    "img": "https://logowik.com/content/uploads/images/fresco6300.jpg",
+    "description": "Optimized restaurant website for enhanced user experience, featuring interactive menus, efficient table reservations, and promotions. Improved customer engagement with streamlined online services.",
+    "link": "https://fresco12.netlify.app/",
+    "github": "https://github.com/vatsalagarwall/fresco-github"
 }]
